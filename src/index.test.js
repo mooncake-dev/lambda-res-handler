ebb161b75c3fc9fbe997f1aa2ac2e299e76e08e1
Daniël Illouz <daniel.illouz@me.com>
2019-04-26 16:14:03 +0200

    Implement res handler module
    Fixes #1

---
+++ src/index.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const createResHandler = require('./');
+
+const CUSTOM_HEADER = 'x-custom-test';
+const CUSTOM_HEADER_VALUE = true;
+const CUSTOM_STATUS_CODE = 201;
+const CUSTOM_BODY = { hello: 'world' };
+
+describe('createResHandler(?defaultHeaders)', () => {
+  test('creates handler', () => {
+    const sendRes = createResHandler();
+
+    expect(typeof sendRes).toBe('object');
+    expect(typeof sendRes.json).toBe('function');
+  });
+
+  describe('sendRes.json(?statusCode, ?body, ?headers)', () => {
+    test('returns response Object', () => {
+      const sendRes = createResHandler();
+      const res = sendRes.json();
+
+      expect(res).toHaveProperty('headers');
+      expect(res).toHaveProperty('statusCode');
+      expect(res).toHaveProperty('body');
+    });
+
+    test('sends custom headers', () => {
+      const sendRes = createResHandler({
+        [CUSTOM_HEADER]: CUSTOM_HEADER_VALUE
+      });
+      const res = sendRes.json();
+
+      expect(res.headers[CUSTOM_HEADER]).toEqual(CUSTOM_HEADER_VALUE);
+    });
+
+    test('sends default status code 200', () => {
+      const sendRes = createResHandler();
+      const res = sendRes.json();
+
+      expect(res.statusCode).toEqual(200);
+    });
+
+    test('sends custom status code', () => {
+      const sendRes = createResHandler();
+      const res = sendRes.json(CUSTOM_STATUS_CODE);
+
+      expect(res.statusCode).toEqual(CUSTOM_STATUS_CODE);
+    });
+
+    test('sends custom body as JSON', () => {
+      const sendRes = createResHandler();
+      const res = sendRes.json(undefined, CUSTOM_BODY);
+
+      expect(res.body).toEqual(JSON.stringify(CUSTOM_BODY));
+    });
+  });
+});
